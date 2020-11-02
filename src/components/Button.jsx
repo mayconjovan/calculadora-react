9fd41f50ddf4f7584a036f451f8fd8c1bc1dc1de
Maycon J Pereira <mayconjovan@gmail.com>
2020-11-02 16:16:14 -0300

    Add buttons and styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react'
 import './Button.css'
 
 export default (props) => {
-   return <button className='button'>{props.label}</button>
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return <button
+        onClick={e => props.click && props.click(props.label)}
+        className={classes}>
+        {props.label}
+    </button>
 }
